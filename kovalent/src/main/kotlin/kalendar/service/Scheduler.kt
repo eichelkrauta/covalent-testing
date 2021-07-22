7eacabed6a157138e1f8e59fd9b7cc22a68b75b2
Adam Eichelkraut <eichelkrautadam@gmail.com>
2021-07-21 23:17:24 -0500

    refactor to share code between recurring daily and weekly

--- kovalent/src/main/kotlin/kalendar/service/Scheduler.kt
+++ kovalent/src/main/kotlin/kalendar/service/Scheduler.kt
@@ -5,21 +5,18 @@ import kalendar.domain.Event
 import java.time.Period
 
 class Scheduler(private val database: EventDatabase) {
-    fun recurringDaily(event: Event, days: Int) {
-        (0 until days).forEach {
+
+    fun recurringDaily(event: Event, days: Int) = recurring(event, Period.ofDays(1), days)
+
+    fun recurringWeekly(event: Event, weeks: Int) = recurring(event, Period.ofWeeks(1), weeks)
+
+    private fun recurring(event: Event, period: Period, times: Int) {
+        (0 until times).forEach {
             database.create(event.copy(
-                starts = event.starts + Period.ofDays(it),
-                ends = event.ends + Period.ofDays(it)
+                    starts = event.starts + period.multipliedBy(it),
+                    ends = event.ends + period.multipliedBy(it)
             ))
         }
     }
 
-    fun recurringWeekly(event: Event, weeks: Int) {
-        (0 until weeks).forEach {
-            database.create(event.copy(
-                starts = event.starts + Period.ofWeeks(it),
-                ends = event.ends + Period.ofWeeks(it)
-            ))
-        }
-    }
 }
